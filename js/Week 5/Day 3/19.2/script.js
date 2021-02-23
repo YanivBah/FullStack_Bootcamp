938441868c2a1d07f66a73393a2db5288f01b056
Yaniv Bahalker <ybahalker@gmail.com>
2021-02-23 18:40:29 +0200

    19.2 final fix

--- js/Week 5/Day 3/19.2/script.js
+++ js/Week 5/Day 3/19.2/script.js
@@ -1,74 +1,60 @@
-const codeBox = document.querySelectorAll('#code');
-const numbercode = document.querySelector('h2');
+const inputs = document.querySelectorAll('#code');
+const number = document.querySelector('h2');
 const submit = document.querySelector('.submit');
 
-const pasteEvent = () => {
-  for (let i = 0; i < codeBox.length ; i++) {
-    codeBox[i].addEventListener('paste', event => {
-      let paste = (event.clipboardData || window.clipboardData).getData('text');
-      paste = paste.replace(/\s/g,'');
-      if (!/\d/.test(paste) || !(paste.length <= 6)) {
-        alert('You can paste only numbers and 6 charcters max!');
-        return event.preventDefault();
-      }
-      const split = paste.split('');
-      for (let y = i, x = 0; y < split.length ; y++,x++) {
-        codeBox[y].value = split[x];
-        codeBox[y].focus();
-      }
-      console.log(paste);
-    })
+const pasteFn = event => {
+  let paste = (event.clipboardData || window.clipboardData).getData('text');
+  paste = paste.replace(/\s/g,'');
+  const regex = /\d/;
+  const current = parseInt(event.currentTarget.getAttribute("data-number"));
+  if (!regex.test(paste) || paste.length > 6) {
+    alert('You can paste only numbers and 6 charcters max!');
+    return event.preventDefault();
+  } 
+  const split = paste.split('');
+  for (let i = 0, x = current; i < split.length;i++,x++) {
+    if (x <= 5) {
+      inputs[x].value = split[i];
+      inputs[x].focus();
+    }
   }
 }
 
-const moveInput = () => {
-  for (let i = 0; i < codeBox.length; i++) {
-    codeBox[i].addEventListener('input', event => {
-      if (/\d/.test(event.data)) {
-        if (event.data !== null && i !== 5) {
-        codeBox[i+1].focus();
-        }
-      } else {
-        i !== 5 ? codeBox[i].value = null : '';
-      }
-      if (i === 5) {
-        setTimeout(() => submit.click(), 100)
-      }
-    })
-    codeBox[i].addEventListener("keydown", event => {
-      if (event.key === 'Backspace' && i !== 0) {
-        event.preventDefault();
-        codeBox[i].value = null;
-        codeBox[i-1].focus();
-      }
-    })
+const moveForward = event => {
+  const regex = /\d/;
+  const current = parseInt(event.currentTarget.getAttribute("data-number"));
+  if (regex.test(event.data)) {
+    event.data !== null && current < 5 ? inputs[current+1].focus() : '';
+  }
+  current === 5 ? setTimeout(() => submit.click(), 100) : '';
+  // Check for more than one character
+  if (inputs[current].value > 1) {
+    inputs[current].value = inputs[current].value.slice(0,1);
   }
 }
 
-const chkCode = () => {
-  const numberVerify =  numbercode.textContent.replace(/\s/g,'');
-  let inputNumbers = '';
-  codeBox.forEach((num) => inputNumbers += num.value);
-  if (numberVerify === inputNumbers && numberVerify.length === 6) {
+const moveBackward = event => {
+  const current = parseInt(event.currentTarget.getAttribute("data-number"));
+  if (event.key === 'Backspace' && current > 0) {
+    event.preventDefault();
+    inputs[current].value = null;
+    inputs[current-1].focus();
+  }
+}
+
+const codeVerification = () => {
+  const code = number.textContent.replace(/\s/g,'');
+  let userInputNum = '';
+  inputs.forEach((num) => userInputNum += num.value);
+  if (code === userInputNum) {
     alert('Very Good!');
     return true;
   } else {
     alert('Sorry this is not correct!');
     return false;
-    }
+  }
 }
 
-submit.addEventListener("submit", event => {
-  if (!chkCode()) {
-    event.preventDefault();
-    window.history.back();
-  }
-})
-
-pasteEvent();
-moveInput();
-
-// codeBox[0].addEventListener('paste', (event) => {
-//   let paste = (event.clipboardData || window.clipboardData).getData('text');
-//   console.log(paste.replace(/\s/g,''));
-// })
+inputs.forEach(el => el.addEventListener('paste', pasteFn));
+inputs.forEach(el => el.addEventListener('input', moveForward));
+inputs.forEach(el => el.addEventListener('keydown', moveBackward));
